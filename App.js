16abcdaa9df7e302b40c5e0897d09f0976a5fbfe
Syed Jahangeer Junaid <34482648+jahangeer010@users.noreply.github.com>
2022-12-11 06:58:34 -0800

    uodated version 12-12-2022

--- App.js
+++ App.js
@@ -1,25 +1,31 @@
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-
+import {store} from './store'
 
 import Home from './Home';
 import Login from './Login';
+import ViewCart from './src/components/ViewCart';
 
 // import { Store } from './store';
 export default function App() {
 
   const Stack = createStackNavigator();
   return (
-    // <Provider store={Store}>
+    <Provider store={store}>
     <NavigationContainer>
     <Stack.Navigator>
-    <Stack.Screen name="Login" component={Login} />
     <Stack.Screen name="Home" component={Home} />
+
+    <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="ViewCart" component={ViewCart} />
+
     </Stack.Navigator>
     </NavigationContainer>
+    </Provider>
     
   );
 }
--- Login.js
+++ Login.js
@@ -1,10 +1,36 @@
-import { View, Text, TouchableOpacity,SafeAreaView } from 'react-native'
+import { Image, View, Text, TouchableOpacity, SafeAreaView } from 'react-native'
 import React from 'react'
+import { TextInput } from 'react-native-paper';
 
-export default function Login({navigation}) {
+export default function Login({ navigation }) {
     return (
-        <View>
-            <TouchableOpacity style={{
+        <SafeAreaView>
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                <Image
+                    source={{
+                        uri: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png',
+                    }}
+                    style={{
+                        width: 200,
+                        height: 200
+                    }}
+                />
+                <Text style={{ fontSize: 30, fontWeight: 'bold', marginVertical: 10 }}>Login Here</Text>
+            </View>
+            <View style={{margin:10}}>
+                <TextInput label='email' />
+
+                <TextInput style={{marginTop:20}} label='Password' />
+            </View>
+
+            <View>
+                <TouchableOpacity>
+                    
+                </TouchableOpacity>
+
+            </View>
+
+            {/* <TouchableOpacity style={{
                 backgroundColor: 'grey',
                 justifyContent: 'center',
                 width: '50%',
@@ -15,7 +41,7 @@ export default function Login({navigation}) {
             onPress={()=>navigation.navigate('Home')}
             >
                 <Text>Go to Home Screen</Text>
-            </TouchableOpacity>
-        </View>
+            </TouchableOpacity> */}
+        </SafeAreaView>
     )
 }
